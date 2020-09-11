dd83629c73509ef93c3a4072e3ad77738eb9c2e2
Shairyar Baig <shairyarbaig@gmail.com>
2020-09-11 12:51:58 +0500

    add ignore route config

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 const { appsignal } = require("./appsignal")
+const { expressMiddleware, expressErrorHandler } = require("@appsignal/express")
 
 const express = require("express");
-const { expressMiddleware, expressErrorHandler } = require("@appsignal/express")
 const app = express();
+
+// ADD THIS AFTER ANY OTHER EXPRESS MIDDLEWARE, BUT BEFORE ANY ROUTES!
 app.use(expressMiddleware(appsignal));
 
 
 app.get("/", (req, res) => {
-  throw new Error('Whoops! 3')
-  // res.send("How are you doing?");
+  // throw new Error('Whoops! error on home page')
+  res.send("How are you doing?");
 });
 
 app.get("/demo", (req, res) => {
   res.send("Demo");  
 });
 
+app.get("/contact", (req, res) => {
+  throw new Error('This error should be igored')
+  res.send("This is a contact page");  
+});
+
+app.get("/login", (req, res) => {
+  // throw new Error('Whoops! error on contact page')
+  res.send("This is a login page");  
+});
+
+app.get("/logout", (req, res) => {
+  // throw new Error('Whoops! error on contact page')
+  res.send("This is a logout page");  
+});
+
+app.get("/ignored", (req, res) => {
+  // throw new Error('Whoops! error on contact page')
+  res.send("This page should be ignored");  
+});
+
+app.get("/faq", (req, res) => {
+  // throw new Error('Whoops! error on contact page')
+  res.send("FAQs");  
+});
+
+// ADD THIS AFTER ANY OTHER EXPRESS MIDDLEWARE, AND AFTER ANY ROUTES!
 app.use(expressErrorHandler(appsignal))
 
 app.listen(8080, () => {
